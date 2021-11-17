df4a9cd98af6b37748b3220a768286121dbe403d
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-11-18 07:12:27 +0900

    first version. not tested

--- mdweek/week.py
+++ mdweek/week.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 from dataclasses import dataclass
 import datetime
 from typing import Union, overload
@@ -70,52 +70,94 @@ class Week:
             return (s1 - s2).days // 7
 
 
-class WeekCalculator(ABC):
+class WeekConfig(ABC):
     @abstractmethod
-    def parse(self, s: str) -> Week:
-        return NotImplemented
-
-    @abstractmethod
-    def to_str(self, w: Week) -> str:
-        return NotImplemented
-
-    @abstractmethod
-    def date(self, w: Week, n: int) -> datetime.date:
+    def first_date(self, year: int) -> datetime.date:
         """
-        その週の特定の曜日を返す。
-        曜日とintの対応は、ISOに準じる。
-        1 = 月曜日, ... , 6 = 土曜日, 7 = 日曜日
+        西暦year年の第１週目を決める日付を返す。
         """
         return NotImplemented
 
-    @abstractmethod
-    def week(self, d: datetime.date) -> Week:
-        return NotImplemented
+    @property
+    def first_dow(self) -> int:
+        """
+        週の始まりが何曜日かを返す。
+        曜日の符号は、ISOに準じて、1=月曜日, ... , 7=日曜日とする。
+        デフォルトでは、月曜日始まりとしておく。
+        """
+        return 1
 
-
-class IsoWeekCalculator(WeekCalculator):
     def parse(self, s: str) -> Week:
-        y, w = s.split("/")
-        return Week(int(y), int(w))
+        y, w = map(int, s.split("/"))
+        return Week(y, w)
 
     def to_str(self, w: Week) -> str:
         return f"{w.year}/{w.week}"
 
-    def date(self, w: Week, n: int) -> datetime.date:
-        if n == 0:
-            n = 7
-        ret = datetime.datetime.fromisocalendar(w.year, w.week, n)
-        return ret.date()
+
+class IsoWeekConfig(WeekConfig):
+    def first_date(self, year: int) -> datetime.date:
+        """
+        西暦year年の最初の木曜日を返す        
+        """
+        jan1 = datetime.date(year, 1, 1)
+        if jan1.isoweekday() <= 4:
+            # year-01-01が月〜木曜日なら、同じ週の木曜日
+            return jan1 + datetime.timedelta(days=4 - jan1.isoweekday())
+        else:
+            # year-01-01が金〜日曜日なら、翌週の木曜日
+            return jan1 + datetime.timedelta(days=7 + 4 - jan1.isoweekday())
+
+
+class WeekCalculator:
+    def parse(self, s: str) -> Week:
+        return _WEEK_CONFIG.parse(s)
+
+    def to_str(self, w: Week) -> str:
+        return _WEEK_CONFIG.to_str(w)
+
+    def move_to_dow(self, d: datetime.date, target_dow: int) -> datetime.date:
+        """
+        同じ週の特定の曜日の日付を返す。
+        """
+        old_dow = d.isoweekday()
+        first_dow = _WEEK_CONFIG.first_dow
+
+        d2 = d + datetime.timedelta(days=first_dow - old_dow)
+        if first_dow <= old_dow:
+            return d2
+        else:
+            return d2 + datetime.timedelta(days=-7)
+
+    def date(self, w: Week, dow: int) -> datetime.date:
+        """
+        w週のdow曜日の日付を返す。
+        """
+        d1 = _WEEK_CONFIG.first_date(w.year)
+        d2 = self.move_to_dow(d1, _WEEK_CONFIG.first_dow)
+        d3 = d2 + datetime.timedelta(days=7 * (w.week - 1))
+        return self.move_to_dow(d3, dow)
 
     def week(self, d: datetime.date) -> Week:
-        d2 = datetime.datetime(d.year, d.month, d.day)
-        iso = d2.isocalendar()
-        return Week(iso.year, iso.week)
+        """
+        日付dに対応するWeekを返す。
+        """
+        d1 = _WEEK_CONFIG.first_date(d.year)
+        if d >= d1:
+            year = d.year
+        else:
+            year = d.year - 1
+        d2 = _WEEK_CONFIG.first_date(year)
+        d3 = self.move_to_dow(d2, _WEEK_CONFIG.first_dow)
+        d4 = self.move_to_dow(d, _WEEK_CONFIG.first_dow)
+        week = (d4 - d3).days // 7
+        return Week(year, week + 1)
 
 
-_WC: WeekCalculator = IsoWeekCalculator()
+_WC: WeekCalculator = WeekCalculator()
+_WEEK_CONFIG = IsoWeekConfig()
 
 
-def setup_week_calculator(wc: WeekCalculator) -> None:
-    global _WC
-    _WC = wc
+def setup_week_config(wc: WeekConfig) -> None:
+    global _WEEK_CONFIG
+    _WEEK_CONFIG = wc
